cb9e79e5961b4440d2b7008235688ccd329629ff
Dnkiss <g1390354456@outlook.com>
2020-12-09 12:04:08 +0800

    OSUAssistant Beta:1.5 更改点击模式算法，使之更为精准

--- src/com/github/Dnkiss/Reader.java
+++ src/com/github/Dnkiss/Reader.java
@@ -8,10 +8,10 @@ import java.nio.charset.StandardCharsets;
 import java.util.*;;
 
 public class Reader {
-    private Queue<Integer> X = new LinkedList<Integer>();
-    private Queue<Integer> Y = new LinkedList<Integer>();
-    private Queue<Long> time = new LinkedList<Long>();
-    private Queue<Long> l = new LinkedList<Long>();
+    private Queue<Integer> X = new LinkedList<>();
+    private Queue<Integer> Y = new LinkedList<>();
+    private Queue<Long> time = new LinkedList<>();
+    private Queue<Long> l = new LinkedList<>();
     private Queue<Character> type = new LinkedList<>(); //n,p,l
 
     Reader(String file){
@@ -43,34 +43,41 @@ public class Reader {
 
             String str;
             Queue<Long> t = new LinkedList<Long>();
-            Queue<Double> bpmt = new LinkedList<Double>();
+            Queue<Double> percent = new LinkedList<Double>();
             BufferedReader br = new BufferedReader(new InputStreamReader
                     (new FileInputStream(new File(file)),
                             StandardCharsets.UTF_8));
             long t1 = 0;
-            long tp = 0;
+            Double tp = Double.valueOf(0);
+            Double sv = Double.valueOf(0);
 
             while((str = br.readLine()) != null){
                 double bpm = 0;
+                if(str.equals("[Difficulty]")){
+                    str = br.readLine();
+                    str = br.readLine();
+                    str = br.readLine();
+                    str = br.readLine();
+                    str = br.readLine();
+                    String[] result = str.split(":");
+                    sv = Double.parseDouble(result[1]);
+                }
                 if(str.equals("[TimingPoints]")){
                     str = br.readLine();
                     String substring = str.substring(0,str.length()-1);
                     String[] result = substring.split(",");
-                    tp = (long)Double.parseDouble(result[1]);
-                    t.add(Long.parseLong(result[0]));
-                    bpm = 60000/tp;
+                    tp = Double.parseDouble(result[1]);
                     while(!(str = br.readLine()).equals("")){
                         substring = str.substring(0,str.length()-1);
                         result = substring.split(",");
                         t.add(Long.parseLong(result[0]));
-                        bpmt.add(100*bpm/Double.parseDouble(result[1]));
+                        percent.add(100/Double.parseDouble(result[1])*-1);
                     }
                 }
                 if(str.equals("[HitObjects]")){
-                    Double n = (double) 0;
+                    Double n = (double) 1;
                     while((str = br.readLine()) != null){
-                        String substring = str.substring(0,str.length()-1);
-                        String[] result = substring.split("\\|");
+                        String[] result = str.split("\\|");
                         String[] r = result[0].split(",");
                         X.add((int)((Float.parseFloat(r[0])+180)*2.2));
                         Y.add((int)((Float.parseFloat(r[1])+82)*2.2));
@@ -80,29 +87,28 @@ public class Reader {
                         time.add(Long.parseLong(r[2]) - t1);
                         if(t.peek()!=null){
                             if(Long.parseLong(r[2]) >= t.peek()){
-                                n = bpmt.poll();
+                                n = percent.poll();
                                 t.poll();
                             }
                         }
                         if(r[5].equals("P")){
                             type.add('p');
-                            result = substring.split(",");
-                            l.add((long)(Double.parseDouble(result[7])*tp*-1 / n));
+                            result = str.split(",");
+                            l.add((long) (Double.parseDouble(result[6])*Double.parseDouble(result[7])*tp/(100*sv*n)));
                         }
                         else if(r[5].equals("L")){
                             type.add('l');
-                            result = substring.split(",");
-                            String[] s = result[5].split("\\|");
-                            for(int i = 0;i < s.length;i++){
-                                String[] re = s[i].split(",");
-                                if(re.length>2){
-                                    l.add((long)(Double.parseDouble(re[2])*tp*-1 / n));
-                                }
-                            }
+                            result = str.split(",");
+                            l.add((long) (Double.parseDouble(result[6])*Double.parseDouble(result[7])*tp/(100*sv*n)));
+                        }
+                        else if(r[5].equals("B")){
+                            type.add('b');
+                            result = str.split(",");
+                            l.add((long) (Double.parseDouble(result[6])*Double.parseDouble(result[7])*tp/(100*sv*n)));
                         }
                         else{
                             type.add('n');
-                            l.add((long) 10);
+                            l.add((long) 1);
                         }
                     }
                 }
